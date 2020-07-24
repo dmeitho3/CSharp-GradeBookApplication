4911bec4178eb0b3b221658c2fe40107e8f88486
Dylan Meithof <dmeitho3@gmail.com>
2020-07-23 17:21:49 -0700

    1

---
+++ GradeBook/Enums/GradeBookT ype.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    class GradeBookT_ype
+    {
+        Standard,
+        Ranked,
+        OneToFour,
+        SixPoint
+    }
+}
