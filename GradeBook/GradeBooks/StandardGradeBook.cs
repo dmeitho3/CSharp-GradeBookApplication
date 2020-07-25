14bff758d9b4712f6ddbc26999904b06cb167202
Dylan Meithof <dmeitho3@gmail.com>
2020-07-24 17:55:06 -0700

    123

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+
+    {
+      StandardGradeBook(string name)
+        :base(name)
+        {
+
+        }
+    }
+}
